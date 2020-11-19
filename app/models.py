d3b701cf68dd65508896df5aa0c0fd1a4e0bcb65
Pongpitta_win_HP <pkrasejun1994@gmail.com>
2020-11-19 16:01:18 +0700

    updating on chaptor 6

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -19,6 +20,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatr.com/avatar/{}?d=identicon%s={}'.format(digest, size)
+    
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
